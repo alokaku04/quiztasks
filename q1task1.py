ed3b0dcb42adf038ccb943039561dfdab22d10bc
alokaku04ls <alok.ranjan@dal.ca>
2017-10-05 10:14:49 -0300

    vlan print1

--- q1task1.py
+++ q1task1.py
@@ -1,6 +1,19 @@
 f=open('running-config.cfg','r')
 
-z=[]
-for line in f:
+x=[]
+y=[]
+def vlan(f):
+ for line in f:
+    line=line.strip()
     if 'vlan' in line:
-     print(line)
+        x.append(line)
+ return x
+
+def interface(f):
+    for line1 in f:
+        line1 = line1.strip()
+        if 'GigabitEthernet' in line1:
+            y.append(line1)
+    return y
+print(vlan(f))
+print(interface(f))
